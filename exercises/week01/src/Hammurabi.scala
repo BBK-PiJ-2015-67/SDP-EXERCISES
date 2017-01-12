0853c8a71c5cf2a4649fbee40d20a321838b8e43
Laurent Mignot <laurent@mignot.eu>
2017-01-12 20:19:20 +0000

    Exercise 2.4, 2.5

--- exercises/week01/src/Hammurabi.scala
+++ exercises/week01/src/Hammurabi.scala
@@ -1,18 +1,18 @@
 object Hammurabi {
 
-    var starved = 0 // how many people starved
-    var immigrants = 5 // how many peopl came to the city
-    var population = 100
-    var harvest = 3000 // total bushels harvested
-    var bushelsPerAcre = 3 // amount harvested for each acre planted
-    var rats_ate = 200 // bushels destroyed by rats
-    var bushelsInStorage = 2800
-    var acresOwned = 1000
-    var pricePerAcre = 19 // each acre costs this many bushels
-    var plagueDeaths = 0
+  var starved = 0 // how many people starved
+  var immigrants = 5 // how many people came to the city
+  var population = 100
+  var harvest = 3000 // total bushels harvested
+  var bushelsPerAcre = 3 // amount harvested for each acre planted
+  var rats_ate = 200 // bushels destroyed by rats
+  var bushelsInStorage = 2800
+  var acresOwned = 1000
+  var pricePerAcre = 19 // each acre costs this many bushels
+  var plagueDeaths = 0
 
-    def printIntroductoryMessage = {
-        println("""
+  def printIntroductoryMessage = {
+    println("""
         Congratulations, you are the newest ruler of ancient Samaria, elected
         for a ten year term of office. Your duties are to dispense food, direct
         farming, and buy and sell land as needed to support your people. Watch
@@ -28,20 +28,25 @@ object Hammurabi {
             * The market price for land fluctuates yearly.
 
         Rule wisely and you will be showered with appreciation at the end of
-        your term. Rule poorly and you will be kicked out of office!""")
-    }
+        your term. Rule poorly and you will be kicked out of office!
+    """)
+  }
 
-    def hammurabi {
-        printIntroductoryMessage
+  def hammurabi = {
+    printIntroductoryMessage
 
-        for (year <- 1 to 10) {
-            println("O great Hammurabi!\n" +
-            "You are in year " + year + " of your ten year rule.\n" +
-            "In the previous year " + starved + "people starved to death.\n" +
-            "In the previous year " + immigrants + "people entered the kingdom.\n" +
-            "The population is now " + population + ".\n" +
-            "We harvested " + harvest + ""
-            )
-        }
+    for (year <- 1 to 10) {
+      println("O great Hammurabi!\n" +
+        "You are in year " + year + " of your ten year rule.\n" +
+        "In the previous year " + starved + " people starved to death.\n" +
+        "In the previous year " + immigrants + " people entered the kingdom.\n" +
+        "The population is now " + population + ".\n" +
+        "We harvested " + harvest + " bushels at " + bushelsPerAcre + " bushels per acre.\n" +
+        "Rats destroyed " + rats_ate + " bushels, leaving " + bushelsInStorage + " bushels in storage.\n" +
+        "The city owns " + acresOwned + " acres of land.\n" +
+        "Land is currently worth " + pricePerAcre + " bushels per acre.\n" +
+        "There were " + plagueDeaths + " deaths from the plague.\n"
+      )
     }
+  }
 }
