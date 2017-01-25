7f71e8dac99dca7afea56ef1a4af7b2ee5f6a081
Laurent Mignot <laurent@mignot.eu>
2017-01-25 20:02:45 +0000

    Exercise 1 commenced

--- exercises/week02/alarmsystem-scala/src/FireSensor.scala
+++ exercises/week02/alarmsystem-scala/src/FireSensor.scala
@@ -1,9 +1,17 @@
-class FireSensor extends Sensor {
-  override def isTriggered: Boolean = false
+import scala.util.Random
 
-  override def getLocation: String = null
+class FireSensor () extends Sensor {
+  private val sensorType: String = "Fire sensor"
+  private val location: String = "Lobby" // this should be passed through in the Constructor???
+  private var batteryPercentage: Double = 100
 
-  override def getSensorType: String = null
+  override def isTriggered: Boolean = Random.nextInt(100) < 5
 
-  override def getBatteryPercentage: Double = -1
+  override def getLocation: String = location
+
+  override def getSensorType: String = sensorType
+
+  override def getBatteryPercentage: Double = {
+    batteryPercentage -= 10; batteryPercentage
+  }
 }
