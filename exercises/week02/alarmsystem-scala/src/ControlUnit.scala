071085f128407ef5030845788c97e3c2282febd7
Laurent Mignot <laurent@mignot.eu>
2017-01-26 13:03:01 +0000

    Exercise 4. - List of sensors created in App object - Pass list of sensors to control unit when creating it

--- exercises/week02/alarmsystem-scala/src/ControlUnit.scala
+++ exercises/week02/alarmsystem-scala/src/ControlUnit.scala
@@ -1,4 +1,4 @@
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable
 
 object ControlUnit
 
@@ -6,11 +6,8 @@ object ControlUnit
 // 1. maintain list of sensors
 // 2. initialise sensors
 // 3. check if sensors are triggered etc..
-class ControlUnit {
+class ControlUnit (sensors: mutable.Buffer[Sensor]) {
   def pollSensors() {
-    val sensors = new ListBuffer[Sensor]()
-    sensors += new FireSensor()
-    sensors += new SmokeSensor()
     for (sensor <- sensors) {
       if (sensor.isTriggered) {
         System.out.println("A " + sensor.getSensorType + " sensor was triggered at " + sensor.getLocation)
