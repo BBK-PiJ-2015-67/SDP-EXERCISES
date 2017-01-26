a9be263f47d236878b5295f8a9711400dbca1217
Laurent Mignot <laurent@mignot.eu>
2017-01-26 11:39:23 +0000

    Week 02, Exercise 2 - Implement SmokeSensor methods

--- exercises/week02/alarmsystem-scala/src/SmokeSensor.scala
+++ exercises/week02/alarmsystem-scala/src/SmokeSensor.scala
@@ -1,9 +1,19 @@
 class SmokeSensor extends Sensor {
-  override def isTriggered: Boolean = false
+  private val ChanceOfTriggering = 10
+  private val BatteryDrainPerPoll = 20
+  private val SensorType: String = "Smoke sensor"
+  private val Location: String = "Canteen"
 
-  override def getLocation: String = null
+  private var batteryPercentage: Double = 100
 
-  override def getSensorType: String = null
+  override def isTriggered: Boolean = Random.nextInt(100) < ChanceOfTriggering
 
-  override def getBatteryPercentage: Double = -1
+  override def getLocation: String = Location
+
+  override def getSensorType: String = SensorType
+
+  override def getBatteryPercentage: Double = {
+    batteryPercentage -= BatteryDrainPerPoll
+    batteryPercentage
+  }
 }
