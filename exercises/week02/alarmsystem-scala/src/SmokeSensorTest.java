7c6984a2531b648615de30a61b655973f8fbf2ee
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-01-30 21:36:29 +0000

    make smoke sensor take random seed in constructor - @TODO: don't like this dependency, investigate alternative method

--- exercises/week02/alarmsystem-scala/src/SmokeSensorTest.java
+++ exercises/week02/alarmsystem-scala/src/SmokeSensorTest.java
@@ -3,31 +3,32 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class SmokeSensorTest {
+  private static final int RANDOM_SEED = 1000;
 
   @Test
   public void testThatIsTriggeredReturnsFalse() {
-    SmokeSensor sensor = new SmokeSensor();
+    SmokeSensor sensor = new SmokeSensor(RANDOM_SEED);
     boolean isTriggered = sensor.isTriggered();
     assertEquals(false, isTriggered);
   }
 
   @Test
   public void testThatGetLocationReturnsNull() {
-    SmokeSensor sensor = new SmokeSensor();
+    SmokeSensor sensor = new SmokeSensor(RANDOM_SEED);
     String location = sensor.getLocation();
     assertEquals(null, location);
   }
 
   @Test
   public void testThatGetSensorTypeReturnsNull() {
-    SmokeSensor sensor = new SmokeSensor();
+    SmokeSensor sensor = new SmokeSensor(RANDOM_SEED);
     String sensorType = sensor.getSensorType();
     assertEquals(null, sensorType);
   }
 
   @Test
   public void testThatGetBatteryPercentageReturnsNegativeOne() {
-    SmokeSensor sensor = new SmokeSensor();
+    SmokeSensor sensor = new SmokeSensor(RANDOM_SEED);
     double batteryPercentage = sensor.getBatteryPercentage();
     assertEquals(-1.0, batteryPercentage, 0.01);
   }
