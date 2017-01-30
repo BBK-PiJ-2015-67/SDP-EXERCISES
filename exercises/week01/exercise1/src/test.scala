f721316184e0cf68984ed51ff6e96c68bdd9b5be
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-01-30 21:34:05 +0000

    make exercise1 test script an object so compiler doesn't complain

--- exercises/week01/exercise1/src/test.scala
+++ exercises/week01/exercise1/src/test.scala
@@ -1,4 +1,6 @@
-println(scala.math.pow(13,2).toInt)
+// Adding an object so the compiler doesn't complain
+object test {
+  println(scala.math.pow(13, 2).toInt)
 
-def isEven(num: Int) = (num % 2) == 0
-
+  def isEven(num: Int): Boolean = (num % 2) == 0
+}
