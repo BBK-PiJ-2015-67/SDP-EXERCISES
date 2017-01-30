073da81f08646f726c1b6349af7206d21fff162b
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-01-30 21:35:06 +0000

    week 1, exercise 3, tests

--- exercises/week01/exercise3/src/NumberPersonalitiesTest.java
+++ exercises/week01/exercise3/src/NumberPersonalitiesTest.java
@@ -1,12 +1,23 @@
-import org.junit.Test;
 
+import org.junit.Test;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class NumberPersonalitiesTest {
 
   @Test
   public void testThatIsPrimeReturnsTrueForPrimeNumber () {
     assertTrue(NumberPersonalities.isPrime(2));
+  }
+
+  @Test
+  public void testThatIsPrimeReturnsFalseForNonPrimeNumber () {
+    assertFalse(NumberPersonalities.isPrime(4));
+  }
+
+  @Test
+  public void testThatIsPrimeReturnsFalseForNegativeNumber () {
+    assertFalse(NumberPersonalities.isPrime(-3));
   }
 
 }
