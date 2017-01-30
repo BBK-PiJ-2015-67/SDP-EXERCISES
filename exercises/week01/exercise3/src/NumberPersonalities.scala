2a6fc976c07995b79c08f2a2073664b1fa6a74db
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-01-30 21:35:26 +0000

    NumberPersonalities isPrime method implementation

--- exercises/week01/exercise3/src/NumberPersonalities.scala
+++ exercises/week01/exercise3/src/NumberPersonalities.scala
@@ -6,7 +6,13 @@ object NumberPersonalities {
 
   def main(args: Array[String]): Unit = ???
 
-  def isPrime(n: Int): Boolean = ???
+  def isPrime(n: Int): Boolean = {
+    if (n <= 1) false
+    else {
+      if (n == 2) true
+      else !(2 until n).exists(n % _ == 0)
+    }
+  }
 
   def isHappy(n: Int): Boolean = ???
 
