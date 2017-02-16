6a3ab70fb39cb4000ee0a34d8272fad664df3cfc
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-02-16 15:12:57 +0000

    implement Person and companion object with apply()

--- exercises/week05/src/main/scala/people/Person.scala
+++ exercises/week05/src/main/scala/people/Person.scala
@@ -1,8 +1,19 @@
 package people
 
 /**
-  * Created by lmignot on 16/02/2017.
+  * Person class
+  *
+  * A person has a first and a last name
   */
-class Person {
+class Person (val firstName: String, val lastName: String)
 
+object Person {
+  /**
+    * Given a String containing a full name, returns a new Person
+    * @param fullName The full name of the Person
+    */
+  def apply(fullName: String): Person = {
+    val nameParts = fullName.split(" ")
+    new Person(nameParts(0), nameParts(1))
+  }
 }
