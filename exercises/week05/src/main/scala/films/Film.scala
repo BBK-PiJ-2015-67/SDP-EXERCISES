5f9a3df478098fa75a53adbb4bc163b4c77304bd
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-02-16 15:45:29 +0000

    implement Film.copy()

--- exercises/week05/src/main/scala/films/Film.scala
+++ exercises/week05/src/main/scala/films/Film.scala
@@ -12,4 +12,11 @@ class Film (val name: String,
   def directorsAge: Int = yearOfRelease - director.yearOfBirth
 
   def isDirectedBy (d: Director): Boolean = d.name == director.name
+
+  def copy (name: String = this.name,
+            yearOfRelease: Int = this.yearOfRelease,
+            imdbRating: Double = this.imdbRating,
+            director: Director = this.director): Film = {
+    new Film(name, yearOfRelease, imdbRating, director)
+  }
 }
