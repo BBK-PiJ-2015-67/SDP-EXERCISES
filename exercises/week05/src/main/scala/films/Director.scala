3632cd47e45856aa268566d9629efc3c164b7ca3
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-02-16 15:50:12 +0000

    implement apply() for Director

--- exercises/week05/src/main/scala/films/Director.scala
+++ exercises/week05/src/main/scala/films/Director.scala
@@ -7,3 +7,8 @@ package films
 class Director (val firstName: String, val lastName: String, val yearOfBirth: Int) {
   def name: String = s"$firstName $lastName"
 }
+
+object Director {
+  def apply(firstName: String, lastName: String, yearOfBirth: Int): Director =
+    new Director(firstName, lastName, yearOfBirth)
+}
