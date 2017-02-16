fc4d01635312303f893d6be8a6fdd74f32d42232
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-02-16 16:11:05 +0000

    implement Director, Film as case classes and remove the redundant boilerplate

--- exercises/week05/src/main/scala/films/Director.scala
+++ exercises/week05/src/main/scala/films/Director.scala
@@ -4,14 +4,11 @@ package films
   * A Director
   * @author lmignot
   */
-class Director (val firstName: String, val lastName: String, val yearOfBirth: Int) {
+case class Director (firstName: String, lastName: String, yearOfBirth: Int) {
   def name: String = s"$firstName $lastName"
 }
 
 object Director {
-  def apply(firstName: String, lastName: String, yearOfBirth: Int): Director =
-    new Director(firstName, lastName, yearOfBirth)
-
   def older(d1: Director, d2: Director): Director =
     if (d1.yearOfBirth < d2.yearOfBirth) d1 else d2
 }
--- exercises/week05/src/main/scala/films/Film.scala
+++ exercises/week05/src/main/scala/films/Film.scala
@@ -4,31 +4,17 @@ package films
   * A Film
   * @author lmignot
   */
-class Film (val name: String,
-            val yearOfRelease: Int,
-            val imdbRating: Double,
-            val director: Director) {
+case class Film (name: String,
+                 yearOfRelease: Int,
+                 imdbRating: Double,
+                 director: Director) {
 
   def directorsAge: Int = yearOfRelease - director.yearOfBirth
 
   def isDirectedBy (d: Director): Boolean = d.name == director.name
-
-  def copy (name: String = this.name,
-            yearOfRelease: Int = this.yearOfRelease,
-            imdbRating: Double = this.imdbRating,
-            director: Director = this.director): Film = {
-    new Film(name, yearOfRelease, imdbRating, director)
-  }
 }
 
 object Film {
-  def apply (name: String,
-             yearOfRelease: Int,
-             imdbRating: Double,
-             director: Director): Film = {
-    new Film(name, yearOfRelease, imdbRating, director)
-  }
-
   def highestRating(f1: Film, f2: Film): Double =
     if (f1.imdbRating > f2.imdbRating) f1.imdbRating else f2.imdbRating
 
