a3ec80f71e3f4efd3f5db1905f3c1d5c5c46ecb5
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-02-16 15:32:48 +0000

    Add FilmTest - change to use scalatest

---
+++ exercises/week05/src/test/scala/FilmTest.scala
@@ -0,0 +1,43 @@
+import org.scalatest.FunSpec
+import films._
+
+/**
+  * @author lmignot
+  */
+class FilmTest extends FunSpec {
+  val eastwood = new Director("Clint", "Eastwood", 1930)
+  val mcTiernan = new Director("John", "McTiernan", 1951)
+  val nolan = new Director("Christopher", "Nolan", 1970)
+  val someGuy = new Director("Just", "Some Guy", 1990)
+
+  val memento = new Film("Memento", 2000, 8.5, nolan)
+  val darkKnight = new Film("Dark Knight", 2008, 9.0, nolan)
+  val inception = new Film("Inception", 2010, 8.8, nolan)
+
+  val highPlainsDrifter = new Film("High Plains Drifter", 1973, 7.7, eastwood)
+  val outlawJoseyWales = new Film("The Outlaw Josey Wales", 1976, 7.9, eastwood)
+  val unforgiven = new Film("Unforgiven", 1992, 8.3, eastwood)
+  val granTorino = new Film("Gran Torino", 2008, 8.2, eastwood)
+  val invictus = new Film("Invictus", 2009, 7.4, eastwood)
+
+  val predator = new Film("Predator", 1987, 7.9, mcTiernan)
+  val dieHard = new Film("Die Hard", 1988, 8.3, mcTiernan)
+  val huntForRedOctober = new Film("The Hunt for Red October", 1990, 7.6, mcTiernan)
+  val thomasCrownAffair = new Film("The Thomas Crown Affair", 1999, 6.8, mcTiernan)
+
+  describe("A Director") {
+    it("should have the correct date of birth") {
+      assert(eastwood.yearOfBirth == 1930)
+    }
+  }
+
+  describe("A Film") {
+    it("should have the correct director") {
+      assert(dieHard.director.name == "John McTiernan")
+    }
+
+    it("isDirectedBy() should return if a film is directed by the provided director") {
+      assert(!invictus.isDirectedBy(nolan))
+    }
+  }
+}
