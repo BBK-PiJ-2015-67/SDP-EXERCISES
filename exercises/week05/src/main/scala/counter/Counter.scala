015eb9ba80dc8537f1698f373bda0d47cd003472
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-02-16 14:07:02 +0000

    refactor Counter as a case class

--- exercises/week05/src/main/scala/counter/Counter.scala
+++ exercises/week05/src/main/scala/counter/Counter.scala
@@ -4,15 +4,15 @@ package counter
   * Counter class for Week 5 - exercise 1
   * @author lmignot
   */
-class Counter (val count: Int) {
 
+case class Counter (count: Int = 0) {
   /**
     * Return a new Counter with the value of this Counter incremented by the
     * provided value which defaults to 1
     * @param x: Int The optional value to increment the Counter by
     * @return The new Counter with an incremented value
     */
-  def inc (x: Int = 1): Counter = new Counter(count + x)
+  def inc (x: Int = 1): Counter = this.copy(count + x)
 
   /**
     * Return a new Counter with the value of this Counter incremented by one
@@ -26,7 +26,7 @@ class Counter (val count: Int) {
     * @param x: Int The optional value to decrement the Counter by
     * @return The new Counter with a decremented value
     */
-  def dec (x: Int = 1): Counter = new Counter(count - x)
+  def dec (x: Int = 1): Counter = this.copy(count - x)
 
   /**
     * Return a new Counter with the value of this Counter decremented by one
