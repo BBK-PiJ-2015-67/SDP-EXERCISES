00f03f2a76fd58128f23b8f07b8fba54ca8efb9b
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-02-16 15:53:29 +0000

    implement Director.older()

--- exercises/week05/src/main/scala/films/Director.scala
+++ exercises/week05/src/main/scala/films/Director.scala
@@ -11,4 +11,7 @@ class Director (val firstName: String, val lastName: String, val yearOfBirth: In
 object Director {
   def apply(firstName: String, lastName: String, yearOfBirth: Int): Director =
     new Director(firstName, lastName, yearOfBirth)
+
+  def older(d1: Director, d2: Director): Director =
+    if (d1.yearOfBirth < d2.yearOfBirth) d1 else d2
 }
