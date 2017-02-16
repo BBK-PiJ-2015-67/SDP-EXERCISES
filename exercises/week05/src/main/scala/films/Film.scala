67f5bcd8cf333351652d55ecb119c0199cef7f66
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-02-16 16:08:37 +0000

    implement oldestDirectorAtTheTime()

--- exercises/week05/src/main/scala/films/Film.scala
+++ exercises/week05/src/main/scala/films/Film.scala
@@ -31,4 +31,7 @@ object Film {
 
   def highestRating(f1: Film, f2: Film): Double =
     if (f1.imdbRating > f2.imdbRating) f1.imdbRating else f2.imdbRating
+
+  def oldestDirectorAtTheTime(d1: Film, d2: Film): Director =
+    if (d1.directorsAge > d2.directorsAge) d1.director else d2.director
 }
