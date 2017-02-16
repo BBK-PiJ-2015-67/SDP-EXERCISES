76840633775984dc28e0bd84eab8fad73426ea6a
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-02-16 15:12:35 +0000

    week 5 - tests for Person

---
+++ exercises/week05/src/main/scala/people/Person.scala
@@ -0,0 +1,8 @@
+package people
+
+/**
+  * Created by lmignot on 16/02/2017.
+  */
+class Person {
+
+}
---
+++ exercises/week05/src/test/scala/PersonTest.scala
@@ -0,0 +1,29 @@
+import org.scalatest.FunSpec
+import people._
+
+class PersonTest extends FunSpec {
+  val FirstName1 = "Wade"
+  val LastName1 = "Wilson"
+  val FullName1 = s"$FirstName1 $LastName1"
+  val FirstName2 = "Peter"
+  val LastName2 = "Quill"
+  val FullName2 = s"$FirstName2 $LastName2"
+  val FirstName3 = "Jessica"
+  val LastName3 = "Jones"
+  val FullName3 = s"$FirstName3 $LastName3"
+  val FirstName4 = "Bruce"
+  val LastName4 = "Wayne"
+  val FullName4 = s"$FirstName3 $LastName4"
+
+  describe("A Person") {
+    it("should have a first and last name") {
+      assert(new Person(FirstName1, LastName1).firstName == FirstName1)
+      assert(new Person(FirstName1, LastName1).lastName == LastName1)
+    }
+
+    it("apply in comp. obj. takes a full name and returns appropriate Person") {
+      assert(Person(FullName2).firstName == FirstName2)
+      assert(Person(FullName2).lastName == LastName2)
+    }
+  }
+}
