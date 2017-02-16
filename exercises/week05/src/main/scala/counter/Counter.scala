fbd19a4ae11b9c4ef80878df4392a9267e28405f
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-02-16 12:03:45 +0000

    Exercise 1b - augment the Counter
    - inc() and dec() accept an Int as a parameter which defaults to 1

--- exercises/week05/src/main/scala/counter/Counter.scala
+++ exercises/week05/src/main/scala/counter/Counter.scala
@@ -1,26 +1,42 @@
 package counter
 
 /**
-  * counter.Counter class for Week 5 - exercise 1
+  * Counter class for Week 5 - exercise 1
   * @author lmignot
   */
-class Counter (n: Int) {
+class Counter (private val n: Int) {
 
   /**
-    * Return a new counter.Counter with the value of this counter.Counter incremented by one
-    * @return The new counter.Counter with an incremented value
+    * Return a new Counter with the value of this Counter incremented by the
+    * provided value which defaults to 1
+    * @param x: Int The optional value to increment the Counter by
+    * @return The new Counter with an incremented value
     */
-  def inc: Counter = new Counter(n + 1)
+  def inc (x: Int = 1): Counter = new Counter(n + x)
 
   /**
-    * Return a new counter.Counter with the value of this counter.Counter decremented by one
-    * @return The new counter.Counter with a decremented value
+    * Return a new Counter with the value of this Counter incremented by one
+    * @return The new Counter with an incremented value
     */
-  def dec: Counter = new Counter(n - 1)
+  def inc: Counter = inc()
 
   /**
-    * Retrieve the value of this counter.Counter
-    * @return an Int containing the value of this counter.Counter
+    * Return a new Counter with the value of this Counter decremented by
+    * the provided value which defaults to 1
+    * @param x: Int The optional value to decrement the Counter by
+    * @return The new Counter with a decremented value
+    */
+  def dec (x: Int = 1): Counter = new Counter(n - x)
+
+  /**
+    * Return a new Counter with the value of this Counter decremented by one
+    * @return The new Counter with a decremented value
+    */
+  def dec: Counter = dec()
+
+  /**
+    * Retrieve the value of this Counter
+    * @return an Int containing the value of this Counter
     */
   def count: Int = n
 }
