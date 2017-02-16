217da3f8e9d3cbab81d4916e719a8dc4a60c4248
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-02-16 14:06:50 +0000

    add test - Counter should have a default value of 0

--- exercises/week05/src/test/scala/CounterTest.scala
+++ exercises/week05/src/test/scala/CounterTest.scala
@@ -4,6 +4,7 @@ import counter._
 class CounterTest extends FunSpec {
   val MinusOne: Int = -1
   val MinusTwo: Int = -2
+  val Zero: Int = 0
   val One: Int = 1
   val Two: Int = 2
   val Three: Int = 3
@@ -17,38 +18,40 @@ class CounterTest extends FunSpec {
   val Fourteen: Int = 14
 
   describe("A Counter") {
+    it(s"should have a default value of $Zero") {
+      assert(Counter().count == 0)
+    }
+
     it("inc() should increment the counter") {
-      assert(new Counter(One).inc.count == Two)
+      assert(Counter(One).inc.count == Two)
     }
 
     it("dec() should decrement the counter") {
-      assert(new Counter(Fourteen).dec.count == Thirteen)
+      assert(Counter(Fourteen).dec.count == Thirteen)
     }
 
     it("inc() should work with negative values") {
-      assert(new Counter(MinusTwo).inc.count == MinusOne)
+      assert(Counter(MinusTwo).inc.count == MinusOne)
     }
 
     it("dec() should work with negative values") {
-      assert(new Counter(MinusOne).dec.count == MinusTwo)
+      assert(Counter(MinusOne).dec.count == MinusTwo)
     }
-  }
-
-  describe("An augmented Counter") {
     it("inc() should accept a value") {
-      assert(new Counter(One).inc(Five).count == Six)
+      assert(Counter(One).inc(Five).count == Six)
     }
 
     it("dec() should accept a value") {
-      assert(new Counter(Fourteen).dec(Five).count == Nine)
+      assert(Counter(Fourteen).dec(Five).count == Nine)
     }
 
     it(s"inc() should default to $One") {
-      assert(new Counter(One).inc.count == Two)
+      assert(Counter(One).inc.count == Two)
     }
 
     it(s"dec() should default to $One") {
-      assert(new Counter(Three).dec.count == Two)
+      assert(Counter(Three).dec.count == Two)
     }
   }
+
 }
