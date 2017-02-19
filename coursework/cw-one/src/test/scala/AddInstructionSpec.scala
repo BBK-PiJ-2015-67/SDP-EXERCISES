3fc69d773d9e0f47dac4c84412b95825c3a98141
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-02-19 14:48:41 +0000

    (cw-one):  More AddInstruction tests

--- coursework/cw-one/src/test/scala/AddInstructionSpec.scala
+++ coursework/cw-one/src/test/scala/AddInstructionSpec.scala
@@ -10,16 +10,18 @@ class AddInstructionSpec extends BaseSpec {
   private final val R1 = 1
   private final val R2 = 2
   private final val R3 = 3
+  private final val ZERO = 0
   private final val AMT = 72
   private final val RES = AMT * 2
 
   describe("An ADD instruction") {
-    val m: Machine = new Machine(Labels(), Vector())
-    val ai: AddInstruction = AddInstruction(Label, R3, R1, R2)
-    m.regs(R1) = AMT
-    m.regs(R2) = AMT
 
     it("should initialise with the correct values") {
+      val m: Machine = new Machine(Labels(), Vector())
+      val ai: AddInstruction = AddInstruction(Label, R3, R1, R2)
+      m.regs(R1) = AMT
+      m.regs(R2) = AMT
+
       ai.label should be (Label)
       ai.op should be (OP)
       ai.result should be (R3)
@@ -29,8 +31,30 @@ class AddInstructionSpec extends BaseSpec {
 
     describe("Executing the instruction") {
       it("should sum the values of 2 registers and store the result in the correct register") {
+        val m: Machine = new Machine(Labels(), Vector())
+        val ai: AddInstruction = AddInstruction(Label, R3, R1, R2)
+        m.regs(R1) = AMT
+        m.regs(R2) = AMT
+
         ai.execute(m)
         m.regs(R3) should be (RES)
+      }
+
+      it("should correctly sum the values given any valid Int value") {
+        val m: Machine = new Machine(Labels(), Vector())
+        val ai: AddInstruction = AddInstruction(Label, R3, R1, R2)
+
+        ai.execute(m)
+        m.regs(R3) should be (ZERO)
+      }
+
+      it("should correctly sum the values given the same register for arguments") {
+        val m: Machine = new Machine(Labels(), Vector())
+        val ai: AddInstruction = AddInstruction(Label, R1, R1, R1)
+        m.regs(R1) = AMT
+
+        ai.execute(m)
+        m.regs(R1) should be (RES)
       }
     }
   }
