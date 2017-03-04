c3361e56603b05e72d59f9ca5b349ab18ad939a9
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-03-04 20:57:48 +0000

    week 07 - modify factory tests

--- exercises/week07/src/test/scala/FactorySpec.scala
+++ exercises/week07/src/test/scala/FactorySpec.scala
@@ -10,39 +10,35 @@ class FactorySpec extends BaseSpec {
 
     it("should create a default Product") {
       Given("A product factory")
-      val creator: Creator = new ConcreteCreator
-
       When("getInstance is called without parameters")
-      val p: Product = creator.create()
+      val p: Product = ConcreteCreator()
 
       Then("A default product should be returned")
       p shouldBe a [ConcreteProduct]
 
       And("It should have a default name")
-      p.getName should be ("Default")
+      p.name should be ("Default")
 
       And("It should have the default cost")
-      p.getCost should be (0.0)
+      p.cost should be (0.0)
     }
 
     it("should create the correct Product") {
       Given("A product factory")
-      val creator: Creator = new ConcreteCreator
-
       When("a Toy is requested")
-      val toy: Product = creator.create("toy")
+      val toy: Product = ConcreteCreator("toy")
 
       Then("A Toy should be created")
       toy shouldBe a [Toy]
 
       When("Food is requested")
-      val pasta: Product = creator.create("food")
+      val pasta: Product = ConcreteCreator("food")
 
       Then("Food should be created")
       pasta shouldBe a [Food]
 
       When("a Tool is requested")
-      val wrench: Product = creator.create("tool")
+      val wrench: Product = ConcreteCreator("tool")
 
       Then("a Tool should be created")
       wrench shouldBe a [Tool]
