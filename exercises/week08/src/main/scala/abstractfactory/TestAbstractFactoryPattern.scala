364a3120795a8a6e74a2b584d0ec6185c34568b5
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-03-05 14:59:55 +0000

    week 08 - abstract factory
    - change test provided
    
    - add some tests

--- exercises/week08/src/main/scala/abstractfactory/TestAbstractFactoryPattern.scala
+++ exercises/week08/src/main/scala/abstractfactory/TestAbstractFactoryPattern.scala
@@ -1,16 +1,17 @@
 package abstractfactory
 
-object TestAbstractFactoryPattern {
-  def main(args: Array[String]) {
-    var parserFactory: AbstractParserFactory = ParserFactoryProducer.getFactory("NYCFactory")
-    var parser: XMLParser = parserFactory.getParserInstance("NYCORDER")
-    var msg: String = ""
-    msg = parser.parse
-    println(msg)
-    println("************************************")
-    parserFactory = ParserFactoryProducer.getFactory("LondonFactory")
-    parser = parserFactory.getParserInstance("LondonFEEDBACK")
-    msg = parser.parse
-    println(msg)
-  }
+object TestAbstractFactoryPattern extends App {
+  val originalMessage = "Parse this XML data using the correct factory."
+
+  val nycParser = ParserFactoryProducer.getFactory("NYCFactory")
+    .flatMap(_.getParserInstance("NYCORDER"))
+  val nycMessage = nycParser.map(_.parse(originalMessage))
+  println(nycMessage)
+
+  println("*"*40)
+
+  val londonParser = ParserFactoryProducer.getFactory("LondonFactory")
+    .flatMap(_.getParserInstance("LondonFEEDBACK"))
+  val londonMessage = londonParser.map(_.parse(originalMessage))
+  println(londonMessage)
 }
--- exercises/week08/src/test/scala/AbstractFactorySpec.scala
+++ exercises/week08/src/test/scala/AbstractFactorySpec.scala
@@ -1,6 +1,42 @@
+import abstractfactory._
+import test.BaseSpec
+
 /**
+  * AbstractFactory tests
   * @author lmignot
   */
-class AbstractFactorySpec {
+class AbstractFactorySpec extends BaseSpec {
+  describe("An abstract factory") {
+    it("abstract factory should return the appropriate factory if available") {
+      val opt: Option[AbstractParserFactory] = ParserFactoryProducer.getFactory("NYCFactory")
+      opt.get shouldBe a [AbstractParserFactory]
+      opt.isDefined should be (true)
+    }
 
+    it("abstract factory should return None if no factory is available") {
+      val opt: Option[AbstractParserFactory] = ParserFactoryProducer.getFactory("asdf")
+      opt shouldBe None
+      opt.isEmpty should be (true)
+    }
+
+    it("if a factory is available, factory should return parser if available") {
+      val msg = "Parse this XML data using the correct factory."
+      val expected = "LondonFEEDBACK: Rctug\"vjku\"ZON\"fcvc\"wukpi\"vjg\"eqttgev\"hcevqt{0"
+      val opt: Option[XMLParser] = ParserFactoryProducer
+        .getFactory("LondonFactory")
+        .flatMap(_.getParserInstance("LondonFEEDBACK"))
+      opt.get shouldBe a [XMLParser]
+      opt.isDefined should be (true)
+      opt.get.parse(msg) should be (expected)
+    }
+
+    it("if a factory is available, factory should return None if no parser is available") {
+      val opt: Option[XMLParser] = ParserFactoryProducer
+        .getFactory("LondonFactory")
+        .flatMap(_.getParserInstance("NYCORDER"))
+      opt shouldBe None
+      opt.isDefined should be (false)
+    }
+
+  }
 }
