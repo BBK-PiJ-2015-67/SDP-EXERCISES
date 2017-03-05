b1982a05666892102ebbb3efdc9c5f1b9c54b216
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-03-05 10:06:01 +0000

    week07 - decorator tests

--- exercises/week07/src/test/scala/DecoratorSpec.scala
+++ exercises/week07/src/test/scala/DecoratorSpec.scala
@@ -1,8 +1,28 @@
 package test
 
+import java.text.DecimalFormat
+
+import decorator._
+
 /**
   * @author lmignot
   */
 class DecoratorSpec extends BaseSpec {
+  private val dFormat: DecimalFormat = new DecimalFormat("#.##")
+
+  private val meatCheeseHamNonVegPrice = dFormat.format(403.09)
+  private val meatCheeseHamNonVegDesc = "SimplyNonVegPizza, Meat (14.25), Cheese (20.72), Ham (18.12)"
+
+  describe("A decorator") {
+    it("should decorate the given class") {
+      Given("a pizza")
+      val simplePizza: Pizza = new SimplyNonVegPizza()
+      When("the pizza is decorated")
+      val decoratedPizza: Pizza = new Ham(new Cheese(new Meat(simplePizza)))
+      Then("the pizza should have the expected toppings")
+      dFormat.format(decoratedPizza.getPrice) should be (meatCheeseHamNonVegPrice)
+      decoratedPizza.getDesc should be (meatCheeseHamNonVegDesc)
+    }
+  }
 
 }
