6e8cd18f1eee7ec7d722fb7de224856436c21c8b
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-03-09 15:08:18 +0000

    akka practice - don't need to check it in to portfolio

--- exercises/week09-10-Akka/src/main/scala/akkanotes/messaging/protocols/TeacherProtocol.scala
+++
@@ -1,9 +0,0 @@
-package akkanotes.messaging.protocols
-
-/**
-  * @author Arun Manivannan
-  */
-object TeacherProtocol {
-  case class QuoteRequest()
-  case class QuoteResponse(quoteString:String)
-}
