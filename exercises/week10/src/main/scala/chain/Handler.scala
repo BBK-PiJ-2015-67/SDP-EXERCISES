6ad46623f808e433aae1dc582ea2e39faff743fb
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-04-27 12:40:44 +0100

    week 10 - COR: document trait and modify return types (for testing)

--- exercises/week10/src/main/scala/chain/Handler.scala
+++ exercises/week10/src/main/scala/chain/Handler.scala
@@ -1,8 +1,23 @@
 package chain
 
 trait Handler {
-	def setHandler(handler: Handler)
-	def process(file: File)
-	def getHandlerName(): String
+  /**
+    * Set the next [[Handler]] in the chain, and returns this [[Handler]] object
+    * to enable method chaining
+    *
+    * @param handler The next [[Handler]] in this chain of responsibility
+    * @return The current object
+    */
+  def setHandler(handler: Handler): Handler
+
+  /**
+    * Process the file, or pass it along to the next [[Handler]] in the chain
+    * if one exists. Outputs an error if not
+    *
+    * @param file The [[File]] to process
+    * @return A success or failure message
+    */
+  def process(file: File): String
+
+  def getHandlerName: String
 }
-    
